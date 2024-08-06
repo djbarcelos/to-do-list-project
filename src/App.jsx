635ec8c67986b7ad4e08c89fb896e35940fb6ef1
djbarcelos <djbarcelos@gmail.com>
2024-08-06 13:05:08 -0300

    feat: adicionado componente de input para novas tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import { Header } from './components/Header';
+import { InputNewTask } from './components/InputNewTask';
+
 import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
-
+      <InputNewTask />
     </div>
   )
 }
--- src/components/InputNewTask.jsx
+++ src/components/InputNewTask.jsx
@@ -1,2 +1,15 @@
+import { PlusCircle } from '@phosphor-icons/react'
+import styles from './InputNewTask.module.css'
 
-export function InputNewTask() { }
+export function InputNewTask() {
+
+    return (
+        <div className={styles.inputNewTask}>
+            <input type="text" placeholder='Adicione uma nova tarefa' />
+            <button>
+                <strong>Criar</strong>
+                <PlusCircle size={20} color='#FFF' />
+            </button>
+        </div>
+    )
+}
